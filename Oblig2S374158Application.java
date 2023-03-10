3b003cbec0da9cad90faf5952be26c6a7b08c3e0
officialmilos <112553226+officialmilos@users.noreply.github.com>
2023-03-10 17:15:47 +0100

    Add files via upload

---
+++ Oblig2S374158Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig2_s374158;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig2S374158Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig2S374158Application.class, args);
+    }
+
+}
